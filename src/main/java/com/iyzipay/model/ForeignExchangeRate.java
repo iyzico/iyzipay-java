5823876bc4dadfd95050466a6afbcd40fcf7ad2b
hakan.oklay <hakan.oklay@iyzico.com>
2016-02-02 12:04:46 +0200

    foreign exchange rate and pecco init-auth samples are added

---
+++ src/main/java/com/iyzipay/model/ForeignExchangeRate.java
@@ -0,0 +1,29 @@
+package com.iyzipay.model;
+
+import com.iyzipay.HttpClient;
+import com.iyzipay.IyzipayResource;
+import com.iyzipay.Options;
+import com.iyzipay.request.ConvertForeignExchangeRateRequest;
+
+import java.math.BigDecimal;
+
+public class ForeignExchangeRate extends IyzipayResource {
+
+    private BigDecimal convertedAmount;
+
+    public static ForeignExchangeRate convert(ConvertForeignExchangeRateRequest request, Options options) {
+        return HttpClient.create().post(options.getBaseUrl() + "/foreign-exchange-rate/convert",
+                getHttpHeaders(request, options),
+                request,
+                ForeignExchangeRate.class)
+                .getBody();
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public void setConvertedAmount(BigDecimal convertedAmount) {
+        this.convertedAmount = convertedAmount;
+    }
+}
---
+++ src/main/java/com/iyzipay/model/PeccoAuth.java
@@ -0,0 +1,26 @@
+package com.iyzipay.model;
+
+import com.iyzipay.HttpClient;
+import com.iyzipay.Options;
+import com.iyzipay.request.CreatePeccoAuthRequest;
+
+public class PeccoAuth extends Payment {
+
+    private String token;
+
+    public static PeccoAuth create(CreatePeccoAuthRequest request, Options options) {
+        return HttpClient.create().post(options.getBaseUrl() + "/payment/iyzipos/pecco/auth",
+                getHttpHeaders(request, options),
+                request,
+                PeccoAuth.class)
+                .getBody();
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
