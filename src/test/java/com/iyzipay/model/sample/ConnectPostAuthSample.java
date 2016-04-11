a8937d0e368610d65478d87ff3dd03d7371b54ee
Sabri Onur Tüzün <onur.tuzun@iyzico.com>
2016-04-11 20:41:28 +0300

    connect post auth sample added

---
+++ src/test/java/com/iyzipay/model/sample/ConnectPostAuthSample.java
@@ -0,0 +1,31 @@
+package com.iyzipay.model.sample;
+
+import com.iyzipay.model.ConnectPaymentPostAuth;
+import com.iyzipay.model.Locale;
+import com.iyzipay.model.Status;
+import com.iyzipay.request.CreatePaymentPostAuthRequest;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class ConnectPostAuthSample extends Sample {
+
+    @Test
+    public void should_post_auth() {
+        CreatePaymentPostAuthRequest request = new CreatePaymentPostAuthRequest();
+        request.setLocale(Locale.TR.getValue());
+        request.setConversationId("123456789");
+        request.setPaymentId("1");
+        request.setIp("85.34.78.112");
+
+        ConnectPaymentPostAuth paymentPostAuth = ConnectPaymentPostAuth.create(request, options);
+
+        System.out.println(paymentPostAuth);
+
+        assertNotNull(paymentPostAuth.getSystemTime());
+        assertEquals(Status.SUCCESS.getValue(), paymentPostAuth.getStatus());
+        assertEquals(Locale.TR.getValue(), paymentPostAuth.getLocale());
+        assertEquals("123456789", paymentPostAuth.getConversationId());
+    }
+}
