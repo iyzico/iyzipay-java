16cb907b4e9296df77118984e5415a7ab7cd0daa
aripddev <dev@aripd.com>
2016-11-07 11:22:44 +0200

    removed logging and throwing iyzipay HttpClientException

--- src/main/java/com/iyzipay/HashGenerator.java
+++ src/main/java/com/iyzipay/HashGenerator.java
@@ -1,9 +1,8 @@
 package com.iyzipay;
 
+import com.iyzipay.exception.HttpClientException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.xml.bind.DatatypeConverter;
 
 public final class HashGenerator {
@@ -11,18 +10,13 @@ public final class HashGenerator {
     private HashGenerator() {
     }
 
-    public static String generateHash(String apiKey, String secretKey, String randomString, Object request) {
+    public static String generateHash(String apiKey, String secretKey, String randomString, Object request) throws HttpClientException, NoSuchAlgorithmException {
         String input = apiKey + randomString + secretKey + request;
         StringBuilder sb = new StringBuilder();
-        byte[] result = null;
-        try {
-            MessageDigest crypt = MessageDigest.getInstance("SHA1");
-            result = crypt.digest(input.getBytes());
-            for (int i = 0; i < result.length; i++) {
-                sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
-            }
-        } catch (NoSuchAlgorithmException ex) {
-            Logger.getLogger(HashGenerator.class.getName()).log(Level.SEVERE, null, ex);
+        MessageDigest crypt = MessageDigest.getInstance("SHA1");
+        byte[] result = crypt.digest(input.getBytes());
+        for (int i = 0; i < result.length; i++) {
+            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
         }
         return DatatypeConverter.printBase64Binary(result);
     }
