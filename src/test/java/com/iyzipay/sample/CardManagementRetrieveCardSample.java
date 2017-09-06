d4f2d51779ac0df3aceef2453b472c5d69647095
Alican Akkus <alican.akkus@iyzico.com>
2017-09-06 11:45:04 +0300

    print response to syso

--- src/test/java/com/iyzipay/sample/CardManagementRetrieveCardSample.java
+++ src/test/java/com/iyzipay/sample/CardManagementRetrieveCardSample.java
@@ -1,10 +1,8 @@
 package com.iyzipay.sample;
 
 import com.iyzipay.model.CardManagementPageCard;
-import com.iyzipay.model.CardManagementPageInitialize;
 import com.iyzipay.model.Locale;
 import com.iyzipay.model.Status;
-import com.iyzipay.request.CreateCardManagementPageInitializeRequest;
 import com.iyzipay.request.RetrieveCardManagementPageCardRequest;
 import org.junit.Test;
 
@@ -17,11 +15,13 @@ public class CardManagementRetrieveCardSample extends Sample {
     @Test
     public void should_retrieve_card_management_page_cards() {
         RetrieveCardManagementPageCardRequest retrieveCardManagementPageCardRequest = new RetrieveCardManagementPageCardRequest();
-        retrieveCardManagementPageCardRequest.setPageToken("page token");
+        retrieveCardManagementPageCardRequest.setPageToken("set page token");
         retrieveCardManagementPageCardRequest.setLocale(Locale.TR.getValue());
         retrieveCardManagementPageCardRequest.setConversationId("123456");
 
         CardManagementPageCard cardManagementPageCard = CardManagementPageCard.retrieve(retrieveCardManagementPageCardRequest, options);
+
+        System.out.println(cardManagementPageCard);
 
         assertEquals(Status.SUCCESS.getValue(), cardManagementPageCard.getStatus());
         assertEquals(Locale.TR.getValue(), cardManagementPageCard.getLocale());
