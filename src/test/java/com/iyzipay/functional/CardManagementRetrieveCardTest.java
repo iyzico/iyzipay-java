411fbaff649f2b231040e87426b1f5cbeeee0b33
Berke Emrecan Arslan <berke@beremaran.com>
2021-01-19 13:29:14 +0300

    Revert "DEV-8493 adds pay with iyzico endpoint and samples (#110)"
    This reverts commit bd5e78821c8e754a230c46f382f495633790c605.

--- src/test/java/com/iyzipay/functional/CardManagementRetrieveCardTest.java
+++ src/test/java/com/iyzipay/functional/CardManagementRetrieveCardTest.java
@@ -11,7 +11,9 @@ import com.iyzipay.request.RetrieveCardManagementPageCardRequest;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 public class CardManagementRetrieveCardTest extends BaseTest {
 
