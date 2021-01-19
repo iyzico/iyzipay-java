bd5e78821c8e754a230c46f382f495633790c605
Berke Emrecan Arslan <berke@beremaran.com>
2021-01-19 13:04:41 +0300

    DEV-8493 adds pay with iyzico endpoint and samples (#110)
    * DEV-8493 adds pay with iyzico initialize endpoint and sample
    
    * DEV-8493 fixes request signing
    
    * DEV-8493 adds marketplace sample for pay with iyzico
    
    * DEV-8493 fixes card retrieval test
    
    * Revert "DEV-8493 fixes card retrieval test"
    
    This reverts commit 5d479ff3

--- src/test/java/com/iyzipay/functional/CardManagementRetrieveCardTest.java
+++ src/test/java/com/iyzipay/functional/CardManagementRetrieveCardTest.java
@@ -11,9 +11,7 @@ import com.iyzipay.request.RetrieveCardManagementPageCardRequest;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 public class CardManagementRetrieveCardTest extends BaseTest {
 
