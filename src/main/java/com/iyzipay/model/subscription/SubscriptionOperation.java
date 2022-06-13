d94a3a818650cac296b52c2b6400b203b99cdd73
Hakan Aslan <hakanaslan90@gmail.com>
2022-06-13 12:07:31 +0300

    added response fields of subscription upgrade operation (#129)
    Co-authored-by: Hakan Aslan <hakan.aslan@iyzico.com>

--- src/main/java/com/iyzipay/model/subscription/SubscriptionOperation.java
+++ src/main/java/com/iyzipay/model/subscription/SubscriptionOperation.java
@@ -1,12 +1,17 @@
 package com.iyzipay.model.subscription;
 
+import com.google.gson.annotations.SerializedName;
 import com.iyzipay.HttpClient;
 import com.iyzipay.IyzipayResource;
 import com.iyzipay.Options;
+import com.iyzipay.model.subscription.resource.SubscriptionUpgradeData;
 import com.iyzipay.request.subscription.SubscriptionOrderOperationRequest;
 import com.iyzipay.request.subscription.UpgradeSubscriptionRequest;
 
 public class SubscriptionOperation extends IyzipayResource {
+
+    @SerializedName("data")
+    private SubscriptionUpgradeData subscriptionUpgradeData;
 
     public static SubscriptionOperation cancel(String subscriptionReferenceCode, Options options) {
         String uri = options.getBaseUrl() + "/v2/subscription/subscriptions/" + subscriptionReferenceCode + "/cancel";
