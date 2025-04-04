e7e688821743855665d0f5721c36e1c74e3aaedc
Abdurrahman Basgoynuk <abdurrahman.basgoynuk@iyzico.com>
2025-04-04 15:14:00 +0300

    DEV-32249 adds complete threeds

--- src/main/java/com/iyzipay/model/marketplace/onboarding/settlementtobalance/SubMerchantOnboardingInitialize.java
+++ src/main/java/com/iyzipay/model/marketplace/onboarding/settlementtobalance/SubMerchantOnboardingInitialize.java
@@ -5,6 +5,7 @@ import com.iyzipay.Options;
 import com.iyzipay.request.SubMerchantOnboardingInitializeRequest;
 
 public class SubMerchantOnboardingInitialize extends SubmerchantOnboardingInitializeResource {
+
     public static SubMerchantOnboardingInitialize initialize(SubMerchantOnboardingInitializeRequest request, Options options) {
         String path = "/onboarding/settlement-to-balance/submerchant";
         return HttpClient.create().post(
